22782227ba5da60aad82b3ce35438f10a7f93f86
Joel Sommers <jsommers@colgate.edu>
2020-01-18 09:18:11 -0500

    update tests

--- name.c
+++ name.c
@@ -6,7 +6,7 @@ int main() {
     char name[64];
     printf("Type a name: ");
     fgets(name, 64, stdin);
-    // name[strlen(name)-1] = '\0'; // squash newline from fgets
+    name[strlen(name)-1] = '\0'; // squash newline from fgets
     printf("Welcome to C, %s\n", name);
     return EXIT_SUCCESS;
 }
